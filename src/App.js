c65976a47068dfd72edbd305f4505cb228e865fc
yasinarafat39 <yasinarafat2557@gmail.com>
2023-01-13 22:29:32 +0600

    heading done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import reactLogo from './assets/icons8-react-native-240.png';
 
 function App() {
   return (
     <div className="App">
 
       <section className='main-section'>
-        ds
+
+        <section className='heading'>
+          <img src={reactLogo} alt="React icon" width="200" height="200" />
+          <h4>Lorem ipsum dolor sit amet <br /> consectetur  adipisicing elit Magni ex.</h4>
+        </section>
+
+
       </section>
 
     </div>
