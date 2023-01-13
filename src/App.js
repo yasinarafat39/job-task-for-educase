268c2a49758fd922610b0a29e9e8b116fb5db94b
yasinarafat39 <yasinarafat2557@gmail.com>
2023-01-13 22:00:58 +0600

    Login page main section demu created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <section className='main-section'>
+        ds
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
